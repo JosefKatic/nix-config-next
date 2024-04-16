a814f4d8d98ec9dc37cdaa5efa81fa428baade9a
Josef Katič <josef@joka00.dev>
2024-04-16 17:53:21 +0200

    Deployment preps

--- pkgs/deploySystem/default.nix
+++ pkgs/deploySystem/default.nix
@@ -6,13 +6,15 @@
   deploy-rs,
   lib,
   git,
-  direnv
+  direnv,
 }:
 writeShellScriptBin "deploySystem" ''
   host="$1"
   mkdir -p /tmp/deploy
   cd /tmp/deploy
+  rm -r .config
    ${lib.getExe git}  -C . pull || git clone https://github.com/JosefKatic/nix-config-next.git .
+
   prefetch-config
 
   if [ -z "$host" ]; then
@@ -20,5 +22,4 @@ writeShellScriptBin "deploySystem" ''
       exit 2
   fi
   ${lib.getExe deploy-rs} .#$host $@
-
 ''
